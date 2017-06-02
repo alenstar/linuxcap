d768e4a842ef5674a780fa4d0579d6601f5d0830
alen <alen@touzi.org>
2017-06-02 15:56:39 +0800

    update CMakeList.txt

---
+++ include/imageprocess.h
@@ -0,0 +1,11 @@
+#ifndef IMAGE_PROCESS_H
+#define IMAGE_PROCESS_H
+#include <opencv2/opencv.hpp>
+//#include <opencv2/highgui.hpp>
+
+void colortransfer(cv::Mat image);
+void colortransfer(cv::Mat image);
+void Optimization(cv::Mat image);
+void hairilization(cv::Mat image);
+
+#endif // IMAGE_PROCESS_H
